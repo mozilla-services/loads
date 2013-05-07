076ab6524d377c40cc288f25870ce22222cf4e24
Alexis Métaireau <alexis@notmyidea.org>
2013-05-07 12:01:47 +1000

    Don't import from requests directly.
    This fix the setup.py.

--- loads/__init__.py
+++ loads/__init__.py
@@ -4,7 +4,6 @@ import warnings
 from unittest import SkipTest
 from unittest.case import _ExpectedFailure, _UnexpectedSuccess
 
-from loads.measure import Session
 from loads import _patch  # NOQA
 
 __version__ = '0.1'
@@ -12,6 +11,7 @@ __version__ = '0.1'
 
 class TestCase(unittest.TestCase):
     def __init__(self, test_name):
+        from loads.measure import Session
         unittest.TestCase.__init__(self, test_name)
         self.session = Session(self)
 
