f33ebd89d5cf0073f60403eb40e990b19a4c4d44
Alexis Métaireau <alexis@notmyidea.org>
2013-05-30 16:40:35 +0200

    The changes to WSGIProxy had been merged upstream.

--- loads/measure.py
+++ loads/measure.py
@@ -7,35 +7,6 @@ from wsgiproxy.requests_client import HttpClient
 from loads.util import dns_resolve
 
 
-class RequestsClient(HttpClient):
-    # We need that while waiting upstream to be merged & released.
-    # https://github.com/gawel/WSGIProxy2/pull/1/files
-
-    default_options = dict(verify=False, allow_redirects=False)
-
-    def __init__(self, session, chunk_size=1024 * 24, **requests_options):
-        options = self.default_options.copy()
-        options.update(requests_options)
-
-        self.options = options
-        self.chunk_size = chunk_size
-        self.session = session
-
-    def __call__(self, uri, method, body, headers):
-        kwargs = self.options.copy()
-        kwargs['headers'] = headers
-        if 'Transfer-Encoding' in headers:
-            del headers['Transfer-Encoding']
-        if headers.get('Content-Length'):
-            kwargs['data'] = body.read(int(headers['Content-Length']))
-        response = self.session.request(method, uri, **kwargs)
-        location = response.headers.get('location') or None
-        status = '%s %s' % (response.status_code, response.reason)
-        headers = [(k.title(), v) for k, v in response.headers.items()]
-        return (status, location, headers,
-                response.iter_content(chunk_size=self.chunk_size))
-
-
 class TestApp(_TestApp):
     """A subclass of webtest.TestApp which uses the requests backend per
     default.
@@ -44,7 +15,7 @@ class TestApp(_TestApp):
         self.session = session
         self.test_result = test_result
 
-        client = RequestsClient(session=self.session)
+        client = HttpClient(session=self.session)
         app = HostProxy(app, client=client)
 
         super(TestApp, self).__init__(app, *args, **kwargs)
