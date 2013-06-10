da9a6260b9d811113b10e6f4ad7cff1fa55cb96c
Tarek Ziade <tarek@ziade.org>
2013-06-10 10:52:40 +0200

    deploying slaves

--- loads/tools/deploy.py
+++ loads/tools/deploy.py
@@ -38,73 +38,87 @@ class Host(object):
         self.client.close()
 
 
+
+def _deploy(host, port, user, password, root, cfg, force=False,
+            endpoint='tcp://127.0.0.1:5555'):
+
+    host = Host(host, port, user, password, root)
+    try:
+        # if it's running let's bypass
+        if not force:
+            cmd = 'cd loads; bin/circusctl --endpoint %s status' % endpoint
+            try:
+                print host.execute(cmd)
+                return
+            except ValueError:
+                pass
+
+        # deploying the latest Loads repo - if needed
+        check = '[ -d "loads" ] && echo 1 || echo 0'
+        res = host.execute(check, prefixed=False).strip()
+
+        if res == '0':
+            cmd = 'git clone https://github.com/mozilla-services/loads'
+            print host.execute(cmd)
+        else:
+            cmd = 'cd loads; git pull'
+            print host.execute(cmd)
+
+        # building the virtualenv in a dedicated tmp file
+        venv = '/usr/local/bin/virtualenv'
+        venv_options = '--no-site-packages .'
+        cmd = 'cd loads; %s %s' % (venv, venv_options)
+        print host.execute(cmd)
+
+        # installing all deps
+        cmd = 'cd loads; bin/python setup.py develop; bin/pip install circus'
+        print host.execute(cmd)
+
+        # stopping any running instance
+        cmd = 'cd loads; bin/circusctl quit'
+        try:
+            print host.execute(cmd)
+        except ValueError:
+            pass
+
+        # now running
+        cmd = 'cd loads; bin/circusd --daemon %s' % cfg
+        print host.execute(cmd)
+
+    finally:
+        host.close()
+
+
 def deploy(master, slaves, ssh):
     """Deploy 1 broker and n agents via ssh, run them and give back the hand
     """
     user = ssh['username']
 
     # deploy the broker
-    host = master['host']
+    master_host = host = master['host']
     print 'Deploying the broker at %s' % host
     port = master.get('port', 22)
     password = master.get('password')
 
-    broker = Host(host, port, user, password, '/tmp/loads-broker')
-    try:
-        # deploying the latest Loads repo - if needed
-        check = '[ -d "loads" ] && echo 1 || echo 0'
-        res = broker.execute(check, prefixed=False).strip()
+    _deploy(host, port, user, password, root='/tmp/loads-broker',
+            cfg='loads.ini')
 
-        if res == '0':
-            cmd = 'git clone https://github.com/mozilla-services/loads'
-            print broker.execute(cmd)
-        else:
-            cmd = 'cd loads; git pull'
-            print broker.execute(cmd)
+    # now deploying slaves
+    for slave in slaves:
+        print 'Deploying slaves at %s' % host
+        host = slave['host']
+        port = slave.get('port', 22)
+        password = slave.get('password')
+        env = {'NUMSLAVES': slave.get('num', 10),
+               'MASTER': master_host}
 
-        # building the virtualenv in a dedicated tmp file
-        venv = '/usr/local/bin/virtualenv'
-        venv_options = '--no-site-packages .'
-        cmd = 'cd loads; %s %s' % (venv, venv_options)
-        print broker.execute(cmd)
-
-        # installing all deps
-        cmd = 'cd loads; bin/python setup.py develop; bin/pip install circus'
-        print broker.execute(cmd)
-
-        # stopping any running instance
-        cmd = 'cd loads; bin/circusctl quit'
-        try:
-            print broker.execute(cmd)
-        except ValueError:
-            pass
-
-        # now running the broker & some slaves
-        cmd = 'cd loads; bin/circusd --daemon loads.ini'
-        print broker.execute(cmd)
-    finally:
-        broker.close()
-
-
-def check(broker):
-    user = ssh['username']
-    host = master['host']
-    port = master.get('port', 22)
-    password = master.get('password')
-
-    broker = Host(host, port, user, password, '/tmp/loads-broker')
-    cmd = 'cd loads; bin/circusctl status'
-    try:
-        print broker.execute(cmd)
-        return True
-    except ValueError:
-        return False
-    broker.close()
+        _deploy(host, port, user, password, root='/tmp/loads-slaves',
+                cfg='slaves.ini', endpoint='tcp://127.0.0.1:5558')
 
 
 if __name__ == '__main__':
     ssh = {'username': 'tarek'}
     master = {'host': 'localhost'}
-    slaves = [{}]
-    if not check(master):
-        deploy(master, slaves, ssh)
+    slaves = [{'host': 'localhost'}]
+
+    deploy(master, slaves, ssh)
