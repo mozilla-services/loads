a72e627bba57984cb66d568cd622e3620eed2195
Alexis Métaireau <alexis@notmyidea.org>
2013-06-18 10:42:56 +0200

    add back some other missing files

---
+++ loads/transport/message.py
@@ -0,0 +1,16 @@
+""" Message class.
+"""
+import json
+
+
+class Message(object):
+
+    def __init__(self, **data):
+        self.data = data
+
+    def serialize(self):
+        return json.dumps(self.data)
+
+    @classmethod
+    def load_from_string(cls, data):
+        return cls(**json.loads(data))
