e1a177ef8f64f2a2ad9a7dc8bd72a9fcf81ca859
Tarek Ziade <tarek@ziade.org>
2013-06-18 12:11:03 +0200

    fixed test_functional

--- loads/tests/test_functional.py
+++ loads/tests/test_functional.py
@@ -6,10 +6,11 @@
 # - normal distributed run
 # - run via nosetest
 # - run with cycles / users
-from unittest import TestCase
+from unittest2 import TestCase
 import sys
 #import subprocess
 import time
+import atexit
 
 import requests
 from gevent import subprocess
@@ -19,15 +20,65 @@ from loads.tests.support import get_runner_args
 from loads.transport.client import Client
 
 
+_RUNNING = False
+_processes = []
+
+
+def _start_cmd(cmd):
+    devnull = open('/dev/null', 'w')
+    process = subprocess.Popen([sys.executable, '-m', cmd],
+                               stdout=devnull, stderr=devnull)
+    _processes.append(process)
+
+
+def start_servers():
+    global _RUNNING
+    if _RUNNING:
+        return
+
+    _start_cmd('loads.transport.broker')
+    for x in range(3):
+        _start_cmd('loads.transport.agent')
+    _start_cmd('loads.examples.echo_server')
+
+    # wait for the echo server to be started
+    tries = 0
+    while True:
+        try:
+            requests.get('http://0.0.0.0:9000')
+            break
+        except requests.ConnectionError:
+            time.sleep(.1)
+            tries += 1
+            if tries > 3:
+                raise
+
+    # wait for the broker to be up with 3 slaves.
+    client = Client()
+    while len(client.list()) != 3:
+        time.sleep(.1)
+
+    client.close()
+    _RUNNING = True
+
+
+def stop_servers():
+    for proc in _processes:
+        try:
+            proc.terminate()
+        except OSError:
+            pass
+
+    _processes[:] = []
+
+
+atexit.register(stop_servers)
+
+
 class FunctionalTest(TestCase):
 
     def setUp(self):
-        devnull = open('/dev/null', 'w')
-        cmd = [sys.executable, '-m', 'loads.examples.echo_server']
-        self._server = subprocess.Popen(cmd, stdout=devnull, stderr=devnull)
-
-    def tearDown(self):
-        self._server.kill()
+        start_servers()
 
     def test_normal_run(self):
         start_runner(get_runner_args(
@@ -41,40 +92,9 @@ class FunctionalTest(TestCase):
 
 
 class DistributedFunctionalTest(TestCase):
-
     def setUp(self):
-        self._processes = []
-        try:
-            self._start_cmd('loads.transport.broker')
-            for x in range(3):
-                self._start_cmd('loads.transport.agent')
-            self._start_cmd('loads.examples.echo_server')
-
-            # wait for the echo server to be started
-            try:
-                requests.get('http://0.0.0.0:9000')
-            except requests.ConnectionError:
-                time.sleep(.1)
-
-            # wait for the broker to be up with 3 slaves.
-            self.client = Client()
-            while len(self.client.list()) != 3:
-                time.sleep(.1)
-        except Exception:
-            self.tearDown()
-            raise
-
-    def _start_cmd(self, cmd):
-        devnull = open('/dev/null', 'w')
-        process = subprocess.Popen([sys.executable, '-m', cmd],
-                                   stdout=devnull, stderr=devnull)
-        self._processes.append(process)
-
-    def tearDown(self):
-        self.client.close()
-        for process in self._processes:
-            process.terminate()
-            process.wait()
+        start_servers()
+        self.client = Client()
 
     def test_distributed_run(self):
         start_runner(get_runner_args(
@@ -87,15 +107,19 @@ class DistributedFunctionalTest(TestCase):
         data = self.client.get_data(runs.keys()[0])
         self.assertTrue(len(data) > 100)
 
-    def _test_distributed_run_duration(self):
+    def test_distributed_run_duration(self):
         args = get_runner_args(
             fqn='loads.examples.test_blog.TestWebSite.test_something',
-            agents=2,
-            output=['null'],
+            agents=1,
+            #output=['null'],
             users=10,
-            duration=10)
+            duration=1)
 
         start_runner(args)
+        time.sleep(1.)
         runs = self.client.list_runs()
-        data = self.client.get_data(runs.keys()[0])
+        try:
+            data = self.client.get_data(runs.keys()[0])
+        except Exception:
+            data = self.client.get_data(runs.keys()[0])
         self.assertTrue(len(data) > 10)
