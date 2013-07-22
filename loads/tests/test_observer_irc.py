4ef5f272e369e6971911563fc6c2d8f5ce5cc74e
Tarek Ziade <tarek@ziade.org>
2013-07-22 12:05:22 +0200

    added a unit test for the irc bot

---
+++ loads/tests/test_observer_irc.py
@@ -0,0 +1,66 @@
+import unittest
+import socket
+import select
+import os
+
+from loads.observers import irc
+
+
+_SOCKETS = []
+
+
+def _select(*args):
+    return _SOCKETS, [], []
+
+
+class FakeSocket(object):
+
+    _file = os.path.join(os.path.dirname(__file__), 'ircdata.txt')
+
+    def __init__(self, *args, **kw):
+        self.sent = []
+        with open(self._file) as f:
+            self.data = list(reversed(f.readlines()))
+
+        _SOCKETS.append(self)
+
+    def bind(self, *args):
+        pass
+    close = shutdown = connect = bind
+
+    def send(self, data):
+        self.sent.append(data)
+
+    def recv(self, *args):
+        if self.data == []:
+            return ''
+        return self.data.pop()
+
+
+class TestIRC(unittest.TestCase):
+
+    def setUp(self):
+        self.old = socket.socket
+        socket.socket = FakeSocket
+        self.old_select = select.select
+        select.select = _select
+
+    def tearDown(self):
+        socket.socket = self.old
+        select.select = self.old_select
+
+    def test_send(self):
+
+        results = 'yeah'
+        irc(results, {})
+
+        # what did we send on IRC
+        wanted = ['NICK loads',
+                  'USER loads 0 * :loads',
+                  'JOIN #services-dev',
+                  'PRIVMSG #services-dev :Test over. yeah',
+                  'QUIT :Bye !',
+                  'QUIT :Connection reset by peer']
+
+        data = [line.strip('\r\n') for line in _SOCKETS[0].sent]
+        self.assertEqual(data, wanted)
