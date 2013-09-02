83636a59a2b69de797673f1701f7b13bd89347fd
Alexis Métaireau <alexis@notmyidea.org>
2013-09-02 12:15:34 +0200

    Add a few tests for the external-runner

--- loads/runners/external.py
+++ loads/runners/external.py
@@ -163,21 +163,21 @@ class ExternalRunner(LocalRunner):
         messages are sent directly to the broker).
         """
 
-        def _process_result(msg):
-            data = json.loads(msg[0])
-            data_type = data.pop('data_type')
-
-            # run_id is only used when in distributed mode, which isn't the
-            # case here, so we get rid of it.
-            data.pop('run_id')
-
-            if hasattr(self.test_result, data_type):
-                method = getattr(self.test_result, data_type)
-                method(**data)
-
         # Actually add a callback to process the results to avoid blocking the
         # receival of messages.
-        self._loop.add_callback(_process_result, msg)
+        self._loop.add_callback(self._process_result, msg)
+
+    def _process_result(self, msg):
+        data = json.loads(msg[0])
+        data_type = data.pop('data_type')
+
+        # run_id is only used when in distributed mode, which isn't the
+        # case here, so we get rid of it.
+        data.pop('run_id')
+
+        if hasattr(self.test_result, data_type):
+            method = getattr(self.test_result, data_type)
+            method(**data)
 
     def _execute(self):
         """Spawn all the tests needed and wait for them to finish.
