5312c691a55a9c93ddbd0533a3dbd5f651186b63
Tarek Ziade <tarek@ziade.org>
2013-09-19 12:35:45 +0200

    covering loads.transport.message.Message

---
+++ loads/tests/test_message.py
@@ -0,0 +1,14 @@
+import json
+import unittest2
+from loads.transport.message import Message
+
+
+class TestMessage(unittest2.TestCase):
+
+    def test_message(self):
+        data = {'1': 2}
+        msg = Message(**data)
+        self.assertEquals(msg.serialize(), json.dumps(data))
+
+        msg = Message.load_from_string(json.dumps(data))
+        self.assertEquals(msg.serialize(), json.dumps(data))
